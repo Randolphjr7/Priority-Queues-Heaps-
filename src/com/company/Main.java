b9c266958e3d44b7cfa2c2d87d87d8170ad4ee78
randolphjr7 <randolphjr7@gmail.com>
2020-06-10 01:05:12 -0400

    implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,41 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Test
+        MinHeap myobj = new MinHeap();
+
+        myobj.add(5);
+        myobj.add(4);
+        myobj.add(2);
+        myobj.add(7);
+        myobj.add(9);
+        myobj.add(10);
+        myobj.add(1);
+        myobj.add(3);
+        myobj.add(6);
+        myobj.add(15);
+
+        System.out.println(myobj);
+
+
+        // error find bug
+        myobj.pull();
+        System.out.println(myobj);
+        myobj.pull();
+        System.out.println(myobj);
+        myobj.pull();
+        System.out.println(myobj);
+        myobj.pull();
+        System.out.println(myobj);
+        myobj.pull();
+        System.out.println(myobj);
+
+
+
+
+
+
+
     }
 }
--- src/com/company/MinHeap.java
+++ src/com/company/MinHeap.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class MinHeap {
 
     private int capacity = 10;
@@ -20,5 +22,74 @@ public class MinHeap {
     private int rightChild(int index) { return items[getRightChildIndex(index)]; }
     private int parent(int index) { return items[getParentIndex(index)]; }
 
+    private void swap(int indexOne, int indexTwo) {
+        int temp = items[indexOne];
+        items[indexOne] = items[indexTwo];
+        items[indexTwo] = temp;
+    }
 
+    private void ensureExtraCapacity() {
+        if(size == capacity) {
+            items = Arrays.copyOf(items, capacity * 2);
+            capacity *= 2;
+        }
+    }
+
+    // returns the first element or root or min element
+    public int peak() {
+        if(size == 0) throw new IllegalStateException();
+        return items[0];
+    }
+
+    // extract first or root or min element
+    public int pull() {
+        if(size == 0) throw new IllegalStateException();
+        int item = items[0];
+        items[0] = items[size - 1];
+        size--;
+        percolateDown();
+        return  item;
+    }
+
+    // add an element
+    public void add(int item) {
+        ensureExtraCapacity();
+        items[size] = item;
+        size++;
+        percolateUp();
+    }
+
+    public void percolateUp() {
+        int index = size - 1;
+        while(hasParent(index) && parent(index) > items[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    public void percolateDown() {
+        int index = 0;
+        while(hasLeftChild(index)) {
+            int smallerChildIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && rightChild(index) < leftChild(index)) {
+                smallerChildIndex = getRightChildIndex(index);
+            }
+
+            if(items[index] < items[smallerChildIndex]) {
+                break;
+            } else {
+                swap(index, smallerChildIndex);
+            }
+            index = smallerChildIndex;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MinHeap{" +
+                "capacity=" + capacity +
+                ", size=" + size +
+                ", items=" + Arrays.toString(items) +
+                '}';
+    }
 }
